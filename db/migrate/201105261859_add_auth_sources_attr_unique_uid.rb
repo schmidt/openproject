7c189e8709da35f25b319fd0283c07230ce0e42a
Stephan Eckardt <s.eckardt@finn.de>
2011-05-26 19:10:10 +0200

    renamed migration for attr_unique_uid

--- db/migrate/201105261859_add_auth_sources_attr_unique_uid.rb
+++ db/migrate/201105261859_add_auth_sources_attr_unique_uid.rb
@@ -1,9 +1,9 @@
 class AddAuthSourcesAttrUniqueUid < ActiveRecord::Migration
   def self.up
-    add_column :auth_source, :attr_unique_uid, :string
+    add_column :auth_sources, :attr_unique_uid, :string
   end
 
   def self.down
-    remove_column :auth_source, :attr_unique_uid
+    remove_column :auth_sources, :attr_unique_uid
   end
 end
