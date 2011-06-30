7c9de160ec4103f3a0e7b0f64e61a2ac7d10bc2e
Felix Schäfer <felix@fachschaften.org>
2011-06-30 18:21:07 +0200

    don't force floats to integers

--- app/models/query.rb
+++ app/models/query.rb
@@ -586,9 +586,9 @@ class Query < ActiveRecord::Base
       sql = "#{db_table}.#{db_field} IS NOT NULL"
       sql << " AND #{db_table}.#{db_field} <> ''" if is_custom_filter
     when ">="
-      sql = "#{db_table}.#{db_field} >= #{value.first.to_i}"
+      sql = "#{db_table}.#{db_field} >= #{value.first.to_f}"
     when "<="
-      sql = "#{db_table}.#{db_field} <= #{value.first.to_i}"
+      sql = "#{db_table}.#{db_field} <= #{value.first.to_f}"
     when "o"
       sql = "#{IssueStatus.table_name}.is_closed=#{connection.quoted_false}" if field == "status_id"
     when "c"
