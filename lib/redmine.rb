8bd31984bf6f7492e7a7bef60d8e0b65c97bb861
Tim Felgentreff <timfelgentreff@gmail.com>
2011-08-06 18:33:54 -0700

    :home_path doesn't work anymore

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -168,7 +168,7 @@ Redmine::AccessControl.map do |map|
 end
 
 Redmine::MenuManager.map :top_menu do |menu|
-  menu.push :home, :home_path
+  menu.push :home, "/"
   menu.push :my_page, { :controller => 'my', :action => 'page' }, :if => Proc.new { User.current.logged? }
   menu.push :projects, { :controller => 'projects', :action => 'index' }, :caption => :label_project_plural
   menu.push :administration, { :controller => 'admin', :action => 'index' }, :if => Proc.new { User.current.admin? }, :last => true
