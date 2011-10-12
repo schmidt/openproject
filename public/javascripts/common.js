1a1b8a48c961a32107e58684b3c1624c9a192c28
jwollert <johannes.wollert@gmail.com>
2011-10-12 15:12:49 +0200

    fixes bug with main menu select box deselection

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -244,7 +244,7 @@ jQuery(document).ready(function($) {
     // trigger an artificial mousedown event
     var parent = $(select).parents('li.drop-down');
     // deselect all options
-    $(select + ":selected").each(function (ix, option) {
+    $(select).find(":selected").each(function (ix, option) {
       $(option).attr("selected", false);
     });
     $(select).chosen({allow_single_deselect:false});
